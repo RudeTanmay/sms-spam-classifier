78ece11400a881ec3ac5078d3e41611d2f9a431b
Tanmay Gangurde <143246915+RudeTanmay@users.noreply.github.com>
2024-04-03 23:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
+
 import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import re
 
